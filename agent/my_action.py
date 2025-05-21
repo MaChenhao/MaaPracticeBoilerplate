4f05f10250a31aadaf2832c1725ea0412d88aa92
Chenhao Ma <machenhao@gmail.com>
2025-05-21 11:41:46 -0400

    Add custom configurations.

--- agent/my_action.py
+++ agent/my_action.py
@@ -1,17 +1,22 @@
 from maa.agent.agent_server import AgentServer
 from maa.custom_action import CustomAction
 from maa.context import Context
+import json, time
 
 
-@AgentServer.custom_action("my_action_111")
+@AgentServer.custom_action("task")
 class MyCustomAction(CustomAction):
-
     def run(
         self,
         context: Context,
         argv: CustomAction.RunArg,
     ) -> bool:
-
-        print("my_action_111 is running!")
-
+        argv: dict = json.loads(argv.custom_action_param)
+        tasks = argv["tasks"]
+        if not isinstance(tasks, list):
+            tasks = [tasks]
+        for task in tasks:
+            if context.run_task(task, argv.get("pipeline_override", {})).status.failed:
+                return False
+            time.sleep(1)
         return True
--- configure.py
+++ configure.py
@@ -1,18 +1,20 @@
 from pathlib import Path
 
-import shutil
+import shutil, os
 
 assets_dir = Path(__file__).parent / "assets"
 
 
 def configure_ocr_model():
     if not (assets_dir / "MaaCommonAssets" / "OCR").exists():
-        print("Please clone this repository completely, don’t miss \"--recursive\", and don’t download the zip package!")
-        print("请完整克隆本仓库，不要漏掉 \"--recursive\"，也不要下载 zip 包！")
+        print(
+            'Please clone this repository completely, don’t miss "--recursive", and don’t download the zip package!'
+        )
+        print('请完整克隆本仓库，不要漏掉 "--recursive"，也不要下载 zip 包！')
         exit(1)
 
     ocr_dir = assets_dir / "resource" / "model" / "ocr"
-    if not ocr_dir.exists():   # copy default OCR model only if dir does not exist
+    if not ocr_dir.exists():  # copy default OCR model only if dir does not exist
         shutil.copytree(
             assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn",
             ocr_dir,
@@ -22,7 +24,15 @@ def configure_ocr_model():
         print("Found existing OCR directory, skipping default OCR model import.")
 
 
+def configure_agent():
+    agent_dir = Path(__file__).parent / "agent"
+    agent_target = assets_dir / "agent"
+    if not agent_target.exists():
+        os.symlink(agent_dir, agent_target, target_is_directory=True)
+
+
 if __name__ == "__main__":
+    configure_agent()
     configure_ocr_model()
 
     print("OCR model configured.")
