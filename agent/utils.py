9c19bf35cfa377a340f1da15dee5bc7d22052a04
Chenhao Ma <machenhao@gmail.com>
2026-04-13 21:48:26 -0400

    Update template

--- agent/utils.py
+++ agent/utils.py
@@ -1,14 +1,8 @@
-import platform, subprocess
+import platform, subprocess, ctypes
 
 
 def MsgBox(msg):
     if platform.system() == "Darwin":
         subprocess.run(["osascript", "-e", f'display alert "{msg}"'])
     else:
-        subprocess.run(
-            [
-                "powershell.exe",
-                "-Command",
-                f'(New-Object -ComObject WScript.Shell).Popup("{msg}", 0, "Action", 0)',
-            ]
-        )
+        ctypes.WinDLL("user32").MessageBoxW(None, msg, "Title", 0x1000)
